34bdc2de0c3af945d3f721df4ecf159d29cf4721
fahim007 <ehsanulbarifahim7@gmail.com>
2024-01-29 00:59:27 +0600

    anything changed?

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-import { Server } from "http";
-let server: Server;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("yeahhoo! databse connected");
-    server = app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
-    });
+    await mongoose.connect(config.database_url as string)
+    console.log('yeahhoo! databse connected')
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.error(error, "oh noo!!! database not connected");
+    console.error(error, 'oh noo!!! database not connected')
   }
 }
 
-main();
+main()
